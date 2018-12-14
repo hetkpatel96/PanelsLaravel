b6dae9ffcadf54b185d02285291d1ba3c9dcc05d
Het patel <hp428@njit.edu>
2018-12-14 15:31:40 -0500

    Routes and view for admin created

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="card">
+                    <div class="card-header">ADMIN Dashboard</div>
+
+                    <div class="card-body">
+                        You are logged in as ADMIN.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
