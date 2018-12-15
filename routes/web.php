9ba79bc47b52ecd9f348fd490d5057340ddc6625
Het patel <hp428@njit.edu>
2018-12-15 10:36:38 -0500

    Made changes in the routes

--- routes/web.php
+++ routes/web.php
@@ -53,17 +53,15 @@ Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
     Route::match(['get', 'post'], '/adminOnlyPage/', 'HomeController@admin');
 });
 
-Route::group(['middleware' => 'App\Http\Middleware\SuperAdminMiddleware'], function()
-{
-    Route::match(['get', 'post'], '/superAdminOnlyPage/', 'HomeController@super_admin');
-});
-
 Route::group(['middleware' => 'App\Http\Middleware\MemberMiddleware'], function()
 {
     Route::match(['get', 'post'], '/memberOnlyPage/', 'HomeController@member');
 });
 
-
+Route::group(['middleware' => 'App\Http\Middleware\SuperAdminMiddleware'], function()
+{
+    Route::match(['get', 'post'], '/superAdminOnlyPage/', 'HomeController@super_admin');
+});
 
 
 
